268f7394d4e83cb968f1c814cc0ee4d0ed6cd9ae
LunaParajuli <parajuliluna8@gmail.com>
2024-11-05 21:08:00 +0545

    f4

--- src/component/Headers.jsx
+++ src/component/Headers.jsx
@@ -1,15 +1,45 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 
 const Headers = () => {
+
+  const loginRef = useRef(null);
+
+
+  useEffect(() => {
+
+    const loginButton = loginRef.current;
+
+    if (loginButton) {
+      loginButton.addEventListener('click', () => {
+        loginButton.style.border = "2px solid yellow";
+      });
+    }
+
+
+  }, []);
+
+
   return (
     <>
+
+
+      <ToastContainer theme='colored' position='top-center' />
+
       <div className="flex justify-around bg-[#f0f3f4]  h-14 items-center shadow shadow-gray-400  fixed top-0 left-0 right-0 overflow-x-hidden z-10">
         <div className="flex h-10 items-center">
           <img src="https://marketplace.canva.com/EAFaFUz4aKo/2/0/1600w/canva-yellow-abstract-cooking-fire-free-logo-JmYWTjUsE-Q.jpg" className="h-full rounded" alt="img" />
           <h2 className="text-2xl ml-3 font-bold">Tastemandu</h2>
         </div>
-        <button className=" bg-black text-yellow-400 px-5  rounded h-7 ">Login</button>
+        <Link to="#">
+          <button id="ilogin"
+            ref={loginRef}
+            className=" bg-black text-yellow-400 px-5  rounded h-7 hover:border-yellow-400 ">Login</button>
+        </Link>
       </div>
 
     </>
--- src/component/userSide/Login.jsx
+++ src/component/userSide/Login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
   return (
     <>
 
-      <div className="flex flex-col items-center justify-center px-6 py-5 mx-auto md:h-screen lg:py-0">
+      <div id="loginpage" className="flex flex-col items-center justify-center px-6 py-5 mx-auto md:h-screen lg:py-0">
         <Link to="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white mt-20">
           <img className="w-8 h-8 mr-2" src="https://marketplace.canva.com/EAFaFUz4aKo/2/0/1600w/canva-yellow-abstract-cooking-fire-free-logo-JmYWTjUsE-Q.jpg" alt="logo" />
 
